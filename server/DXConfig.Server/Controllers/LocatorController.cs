9f78a6b818f2c4e8749e657dd00b63ed7a44da89
Fabricio Catae <fcatae@microsoft.com>
2017-10-05 18:20:09 -0300

    add locator

---
+++ server/DXConfig.Server/Controllers/LocatorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DXConfig.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocatorController : Controller
+    {
+        // GET api/locator/myapplication?env=test
+        [HttpGet("{appid}")]
+        public string Get(string appid, [FromQuery]string env)
+        {
+            string optEnvironment = env;
+
+            if (appid == null)
+                throw new ArgumentNullException("appid");
+
+            string appToken = appid;
+
+            if( optEnvironment != null )
+            {
+                appToken = $"{appid}/{optEnvironment}";
+            }
+
+            return appToken;
+        }
+    }
+}
