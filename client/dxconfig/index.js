26ecd72f3a1dd86f03e211c57b5da724e97e8323
Fabricio Catae <fcatae@microsoft.com>
2017-10-05 14:31:58 -0300

    rework client structure

--- client/dxconfig/index.js
+++
@@ -1,1 +0,0 @@
-var dxconfig = require('./bin/client.js');
--- client/dxconfig/src/client.ts
+++ client/dxconfig/src/client.ts
@@ -1,1 +1,31 @@
-console.log('dxconfig v1.0');
+console.log('dxconfig v0.1');
+
+import path = require('path');
+import http = require('http');
+import fs = require('fs');
+
+var endpoint = 'http://localhost:5000/api/config';
+var application = 'myapp001';
+
+var downloadUrl = resolveName(endpoint, application);
+var localFilename = resolveFile(application);
+
+downloadFile(downloadUrl, localFilename);
+
+// resolve the name
+function resolveName(url, filename) {
+    return url + '/' + filename;
+}
+
+// resolve the name
+function resolveFile(filename) {
+    return filename + '.json';
+}
+
+// download the file
+function downloadFile(url, filename) {
+    var file = fs.createWriteStream(filename);
+    var request = http.get(url, function(response) {
+      response.pipe(file);
+    });    
+}
