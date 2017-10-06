c8fd30332ff7b0cdf03d4b5599ee482d33203ea4
Fabricio Catae <fcatae@microsoft.com>
2017-10-06 10:55:01 -0300

    refactor code

---
+++ server/DXConfig.Server/Services/IStoreServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DXConfig.Server.Services
+{
+    public interface IStoreServices
+    {
+        void Write(string containerName, IData containerData, IPasskey key);
+        IData Read(string containerName, IPasskey key);
+    }
+}
