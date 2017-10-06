7d5af676ed3d27fa45a4e22809c62403c1968356
Fabricio Catae <fcatae@microsoft.com>
2017-10-06 12:50:38 -0300

    add razor pages

---
+++ server/DXConfig.Server/Controllers/PortalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DXConfig.Server.Controllers
+{
+    [Route("portal")]
+    public class PortalController : Controller
+    {
+        // GET: Portal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Portal/Details
+        [Route("details")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }        
+    }
+}
