76274060d6f361895f7fcf00516892885a0135ae
Fabricio Catae <fcatae@microsoft.com>
2017-10-06 13:47:22 -0300

    add logout

---
+++ server/DXConfig.Server/Pages/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DXConfig.Server.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public string Message { get; set; }
+        
+        public async Task OnGetAsync()
+        {
+            await HttpContext.SignOutAsync("dxConfigCookieAuthScheme");
+
+            Message = "Your application description page.";
+        }
+    }
+}
