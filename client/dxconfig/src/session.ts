50b3c8596fe98f096a693766e50cb93a3089079c
Fabricio Catae <fcatae@microsoft.com>
2017-10-10 18:37:42 -0300

    save session to disk

--- client/dxconfig/src/session.ts
+++ client/dxconfig/src/session.ts
@@ -11,13 +11,58 @@ var arg0 = process.argv[2];
 initSession(arg0)
 
 function initSession(initParam: string) {
-    var comps = initParam.split('@');
-    var jwtToken = comps[0];
-    var endpoint = comps[1];
+    // var comps = initParam.split('@');
+    // var jwtToken = comps[0];
+    // var endpoint = comps[1];
     
     console.log('initSession');
+    // console.log(endpoint);
+    // console.log(jwtToken);
+
+    var filename = getDxUserConfiguration();
+    //saveConfig(filename, endpoint, jwtToken);
+
+    var config = loadConfig(filename);
+    //testSession(endpoint, jwtToken);
+
+    var jwtToken = config.token;
+    var endpoint = config.endpoint;
+
+    // console.log(endpoint);
+    // console.log(jwtToken);    
 
     testSession(endpoint, jwtToken);
+}
+
+function getDxConfigHomeDir() {
+    // if Windows
+    if( process.env.LOCALAPPDATA ) {
+        return path.join(process.env.LOCALAPPDATA, 'DXConfig');
+    } 
+    
+    // if Unix    
+    return path.join(os.homedir(), '.dxconfig');    
+}
+
+function getDxUserConfiguration() {
+    var filename = 'configserver.json';
+    // if Windows
+    if( process.env.LOCALAPPDATA ) {
+        return path.join(process.env.LOCALAPPDATA, 'DXConfig', filename);
+    } 
+    
+    // if Unix    
+    return path.join(os.homedir(), '.dxconfig', filename);    
+}
+
+function saveConfig(filename, endpoint, jwtToken) {
+    var data = JSON.stringify({ endpoint: endpoint, token: jwtToken });
+    fs.writeFileSync(filename, data);
+}
+
+function loadConfig(filename) {
+    var data = fs.readFileSync(filename);
+    return JSON.parse(data);
 }
 
 function testSession(endpoint, jwtToken) {
