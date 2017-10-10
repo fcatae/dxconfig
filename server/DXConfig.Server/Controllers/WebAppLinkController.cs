697a0a8e6d5d3643fd31dfe5d5458e8595504d36
Fabricio Catae <fcatae@microsoft.com>
2017-10-09 22:13:09 -0300

    webapplink with permission

--- server/DXConfig.Server/Controllers/WebAppLinkController.cs
+++ server/DXConfig.Server/Controllers/WebAppLinkController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DXConfig.Server.Infra;
+using DXConfig.Server.Managers;
+using DXConfig.Server.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,18 +15,32 @@ namespace DXConfig.Server.Controllers
     public class WebAppLinkController : Controller
     {
         private readonly IUserAccessHandler _userAccess;
+        private readonly IConfigServerManager<AppLink> _configServer;
+        private readonly IStorageManager _storageManager;
 
-        public WebAppLinkController(IUserAccessHandler userAccess)
+        public WebAppLinkController(IConfigServerManager<AppLink> configServer, IStorageManager storageManager, IUserAccessHandler userAccess)
         {
             this._userAccess = userAccess;
+            this._configServer = configServer;
+            this._storageManager = storageManager;
         }
 
         // GET: Applink
         public string Show([FromQuery]string link)
         {
             var user = _userAccess.GetUser();
-            
-            return $"applink:{user.Name},{link}";
+            var appResource = new AppLink(link);
+
+            var data = _configServer.Retrieve(user, appResource);
+
+            if (data == null)
+                return "NotFound()";
+
+            string container = data.ToString();
+
+            var config = _storageManager.Read(user, container);
+
+            return config.ToString();
         }
 
         // GET: Applink
