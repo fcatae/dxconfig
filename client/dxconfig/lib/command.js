433e91654cc5d045474d8d3eb1abd9eb62c4cccb
Fabricio Catae <fcatae@microsoft.com>
2017-10-11 11:54:57 -0300

    implement command with help

--- client/dxconfig/lib/command.js
+++ client/dxconfig/lib/command.js
@@ -1,18 +1,30 @@
 "use strict";
-require('yargs')
-    .command(['start [app]', 'run', 'up'], 'Start up an app', {}, function (argv) {
-    console.log('starting up the', argv.app || 'default', 'app');
+var argv = require('yargs')
+    .command('login [token]', 'Log on configuration server', {}, function (argv) {
+    console.log(argv._[0] + ': Not implemented yet');
+})
+    .command('init', 'Create a DX configuration file', {}, function (argv) {
+    console.log(argv._[0] + ': Not implemented yet');
+})
+    .command('add <file>', 'Add secret file', {}, function (argv) {
+    console.log(argv._[0] + ': Not implemented yet');
+})
+    .command('push', 'Update remote configuration server', {}, function (argv) {
+    console.log(argv._[0] + ': Not implemented yet');
+})
+    .command('pull', 'Fetch configuration from remote server', {}, function (argv) {
+    console.log(argv._[0] + ': Not implemented yet');
 })
     .command({
-    command: 'configure <key> [value]',
-    aliases: ['config', 'cfg'],
-    desc: 'Set a config variable',
+    command: 'exemple <key> [value]',
+    aliases: ['e'],
+    desc: 'Example command',
     builder: function (yargs) { return yargs.default('value', 'true'); },
     handler: function (argv) {
-        console.log("setting " + argv.key + " to " + argv.value);
+        console.log(argv._[0] + ': Not implemented yet');
+        console.log("Example setting " + argv.key + " to " + argv.value);
     }
 })
     .demandCommand()
     .help()
-    .wrap(72)
     .argv;
